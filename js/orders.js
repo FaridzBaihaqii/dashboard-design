93f69cb5ddddb4f35e06c05178a23f0ca7a8d3ef
FaridzBaihaqii <muhammadfaridzbaihaqii@gmail.com>
2023-11-10 18:43:22 +0700

    mulai

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        NamaOrderan: 'Baju Erigo',
+        NomorHandphone: '+62 812-1186-4353',
+        Pembayaran: 'BCA',
+        status: 'Sedang Dikemas'
+    },
+    {
+        NamaOrderan: 'Hoodie Erigo',
+        NomorHandphone: '+62 812-5443-7138',
+        Pembayaran: 'COD',
+        status: 'Ditukar'
+    },
+    {
+        NamaOrderan: 'Jaket Varsity',
+        NomorHandphone: '+62 812-2386-4373',
+        Pembayaran: 'BRI',
+        status: 'Sudah Diterima'
+    },
+]
